51c5727c541e6cb9ed42f38871d843a2b78b0e74
Nur Hasin Ahammad <nur.hasin.ahammad@gmail.com>
2025-10-16 09:12:04 +0600

    WIP: save local changes before merging upstream

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { inject } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
+import { ThemeProvider } from './Context/theme.jsx'
 
 // Initialize Vercel Analytics
 inject();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <ThemeProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ThemeProvider>,
 )
